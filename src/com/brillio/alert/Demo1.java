a383155d3a8a1b355d29c8cd7bfe3c3497430af7
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-27 10:40:56 +0530

    alert and explicit wait

---
+++ src/com/brillio/alert/Demo1.java
@@ -0,0 +1,39 @@
+package com.brillio.alert;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm");
+		
+		//click on go
+		driver.findElement(By.xpath("//img[@alt='Go']")).click();
+		
+		//wait for alert to present - 50s 
+
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(50));
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+		String alertText=driver.switchTo().alert().getText();
+		System.out.println(alertText);
+		
+		driver.switchTo().alert().accept();
+	}
+}
+
+
+
